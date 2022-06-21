b4bc14a0b55d85ad95292c3fd65b484788a8ffc7
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-21 19:08:45 +0100

    cra, storybook, tailwind, daisyui base install

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <div className="bg-purple-500">
+          <h1 className="text-xl">Hello World</h1>
+          <button className="btn">Click</button>
+      </div>
+  );
+}
+
+export default App;
