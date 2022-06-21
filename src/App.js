5081a6140ab966e698078b38dd6224a458ee7d2f
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-21 20:23:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/guestNavbar';
+import Home from './pages/Home';
 
 function App() {
  return (
   <Router>
    <div className="flex flex-col justify-between bg-base-100" data-theme="dark">
     <Navbar />
-    <div className="container">
-    <main>Content</main>
-    </div>
+    <main className="container mx-auto px-3 pb-12">
+     <Routes>
+      <Route path="/" element={<Home />} />
+     </Routes>
+    </main>
    </div>
   </Router>
  );
