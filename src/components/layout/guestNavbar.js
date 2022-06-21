50e62845f9d6fbc45047ed05ddde6609ee8cf4d9
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-21 20:23:09 +0100

    Update guestNavbar.js

--- src/components/layout/guestNavbar.js
+++ src/components/layout/guestNavbar.js
@@ -1,22 +1,28 @@
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 function guestNavbar() {
     return (
         <nav className="navbar bg-base-200 text-base-content">
-            <div className="flex-none px-2 mx-2">
-                <Link to="/" className="text-lg font-bold align-middle">
-                    Filmtrack
-                </Link>
+            <div class="navbar-start">
+                <Link to="/" className="btn btn-ghost normal-case text-xl">Filmtrack</Link>
             </div>
-            <div className="flex-1 px-2 mx-2">
-                <div className="flex justify-end">
-                    <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
-                        Home
-                    </Link>
-                    <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
-                        About
-                    </Link>
+            <div class="navbar-end">
+                <Link to="/" className="btn btn-ghost mr-2 hidden lg:flex">
+                    Home
+                </Link>
+                <Link to="/" className="btn btn-ghost mr-2 hidden lg:flex">
+                    About
+                </Link>
+                <a class="btn mr-2">Login</a>
+                <a class="btn btn-primary mr-2">Register</a>
+                <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">About</Link></li>
+                    </ul>
                 </div>
             </div>
         </nav>
