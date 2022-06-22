ad88c5e9de09d670f866dd43225ede71415d582b
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-22 19:22:22 +0100

    navbar redesign & active links

--- src/components/layout/guestNavbar.js
+++ src/components/layout/guestNavbar.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function guestNavbar() {
     return (
@@ -6,13 +6,15 @@ function guestNavbar() {
             <div className="navbar-start">
                 <Link to="/" className="btn btn-ghost normal-case text-xl">Filmtrack</Link>
             </div>
-            <div className="navbar-end">
-                <Link to="/" className="btn btn-ghost mr-2 hidden lg:flex">
+            <div className="navbar-middle">
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'btn btn-ghost btn-active mr-2 hidden lg:flex' : 'btn btn-ghost mr-2 hidden lg:flex')}>
                     Home
-                </Link>
-                <Link to="/about" className="btn btn-ghost mr-2 hidden lg:flex">
+                </NavLink>
+                <NavLink to="/about" className={({ isActive }) => (isActive ? 'btn btn-ghost btn-active mr-2 hidden lg:flex' : 'btn btn-ghost mr-2 hidden lg:flex')}>
                     About
-                </Link>
+                </NavLink>
+                </div>
+            <div className="navbar-end">
                 <a className="btn mr-2">Login</a>
                 <a className="btn btn-primary mr-2">Register</a>
                 <div className="dropdown dropdown-end">
@@ -20,8 +22,8 @@ function guestNavbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
+                        <li><NavLink to="/" className={({ isActive }) => (isActive ? 'btn btn-ghost btn-active mb-2' : 'btn btn-ghost mb-2')}>Home</NavLink></li>
+                        <li><NavLink to="/about" className={({ isActive }) => (isActive ? 'btn btn-ghost btn-active mb-2' : 'btn btn-ghost mb-2')}>About</NavLink></li>
                     </ul>
                 </div>
             </div>
