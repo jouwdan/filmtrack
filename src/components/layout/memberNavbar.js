0834a67a90e7531411ecdad35c518efdc5d4ec5c
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-23 17:30:06 +0100

    logged in user dropdown

--- src/components/layout/memberNavbar.js
+++ src/components/layout/memberNavbar.js
@@ -16,14 +16,21 @@ function memberNavbar() {
                 </NavLink>
                 </div>
             <div className="navbar-end">
-                <a className="btn mr-2" onClick={logout}>Log Out</a>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex="0" className="btn btn-ghost">
+                    <Icon icon="heroicons-solid:user" />
+                    </label>
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a className="btn btn-ghost">My Account</a></li>
+                    <li><a className="btn btn-ghost" onClick={logout}>Log Out</a></li>
+                    </ul>
+                </div>
                 <div className="dropdown dropdown-end">
                     <label tabIndex="0" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><NavLink to="/" className={({ isActive }) => (isActive ? 'btn btn-ghost btn-active mb-2' : 'btn btn-ghost mb-2')}>Home</NavLink></li>
-                        <li><NavLink to="/about" className={({ isActive }) => (isActive ? 'btn btn-ghost btn-active mb-2' : 'btn btn-ghost mb-2')}>About</NavLink></li>
+                        <li><NavLink to="/dashboard" className={({ isActive }) => (isActive ? 'btn btn-ghost btn-active mb-2' : 'btn btn-ghost mb-2')}>Dashboard</NavLink></li>
                     </ul>
                 </div>
             </div>
