d8b9e8d5f68d8aa910cf76bbb3f10ea01bb556b4
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-24 20:19:32 +0100

    favourite icon goes red when movie favourited

--- src/components/cards/movieCard.js
+++ src/components/cards/movieCard.js
@@ -24,15 +24,15 @@ export default function MovieCard(props) {
             : `http://via.placeholder.com/500x750`
         } alt="Movie Poster" className="pt-10" /></figure>
             <div className="card-body w-72 content-center">
-                <h2 className="card-title">{
-        movie.favourite ? (
-            <Icon icon="heroicons-solid:heart" />
-        ) : null
-      }&nbsp;{movie.title}</h2>
+                <h2 className="card-title">{movie.title}</h2>
                 <div><span className="badge badge-success"><Icon icon="heroicons-solid:calendar" /> &nbsp; {movie.release_date}</span> &nbsp;
                 <span className="badge badge-warning"><Icon icon="heroicons-solid:star" /> {movie.vote_average}</span></div>
                 <div className="card-actions">
-                        <button className="btn btn-error" onClick={handleAddToFavourite}><Icon icon="heroicons-solid:heart" /></button>
+                {
+        movie.favourite ? (
+            <button className="btn btn-error" onClick={handleAddToFavourite}><Icon icon="heroicons-solid:heart" /></button>
+            ) : <button className="btn" onClick={handleAddToFavourite}><Icon icon="heroicons-solid:heart" /></button>
+          }
                     <Link to={`/movies/${movie.id}`}>
                         <button className="btn btn-primary">More Info</button>
                     </Link>
